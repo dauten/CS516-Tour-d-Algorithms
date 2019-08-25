1d27bfa75f921b5bd5a7ac6ea2a786871125dbc3
Dale Auten <dauten96@gmail.com>
2019-08-24 19:58:53 -0500

    bucket sort implemented, but memory issues

--- PR01/bbs.cpp
+++ PR01/bbs.cpp
@@ -4,9 +4,10 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
 
 
-int partition(int* array, int low, int high){
+int partition(std::vector<int> array, int low, int high){
     int pivot = array[high]; //set last item as pivot
     int i = low;
     int t = 0;
@@ -25,7 +26,7 @@ int partition(int* array, int low, int high){
     return i;
 }
 
-void quickSort(int* array, int low, int high){
+void quickSort(std::vector<int> array, int low, int high){
 
   if( low < high )
   {
@@ -33,6 +34,77 @@ void quickSort(int* array, int low, int high){
     quickSort(array, low, p-1);
     quickSort(array, p+1, high);
   }
+}
+
+
+std::vector<int> bubbleSort(std::vector<int> vector){
+  int b = 0;
+  while(!b)
+  {
+    b = 1;
+    for(int i = 0; i < vector.size()-1; i++)
+    {
+      if(vector[i+1] < vector[i])
+      {
+        int t = vector[i];
+        vector[i] = vector[i+1];
+        vector[i+1] = t;
+        b = 0;
+      }
+    }
+  }
+  return vector;
+}
+
+int* bucketSort(int* array, int k, int size){
+    printf("int size is %d\n", sizeof(int));
+    printf("l size is %d\n", sizeof(long));
+    printf("ll size is %d\n", sizeof(long long unsigned int));
+    std::vector<int> buckets[k];  //make k buckets
+
+    printf("allocated vector\n");
+    int i = 0;
+    int max = -1;
+    for(;i < size; i++) //find largest item in array to help determine each bucket's range
+    {
+      if(array[i] > max)
+        max = array[i];
+    }
+
+    max++;
+
+    printf("beginning distributing\n");
+
+    for(i = 0; i < size; i++)
+    {
+      long long unsigned int index = k*((double)array[i]/max);
+
+    //  printf("adding to index:%d\n", index);
+      buckets[index].push_back(array[i]); //insert item into appropriate bucket
+    }
+
+    printf("beginning with sorting\n");
+
+    for(i = 0; i < k; i++){
+
+      if(buckets[i].size() > 1)
+        buckets[i] = bubbleSort(buckets[i]);
+
+    }
+
+    printf("done with sorting\n");
+
+    int t = 0;
+    for(i = 0; i < k; i++){
+      for(int y = 0; y < buckets[i].size(); y++){
+
+        array[t] = buckets[i][y];
+        t++;
+      }
+    }
+
+    return array;
+
 }
 
 // create an array of length size of random numbers// returns a pointer to the array3
@@ -74,12 +146,12 @@ int main(int argc, char** argv )
   // get the random numbers
   array = randNumArray( size, seed );
 
-
+/*
   for(int i = 0; i < size; i++)
   {
     printf("%d ", array[i]);
   }
-
+*/
   //**************************/
   /**************************/
   /**************************/
@@ -89,15 +161,23 @@ int main(int argc, char** argv )
   /**************************/
   printf("\n");
   //free(array);
-  quickSort(array, 0, size-1);
+  int bsize = 0;
+  if(size < 10000){
+    bsize = size/10;
+  }
+  else{
+    bsize = 4000;
+  }
+  array = bucketSort(array, size/10, size);
 
   printf("\n");
 
+/*
   for(int i = 0; i < size; i++)
   {
     printf("%d ", array[i]);
   }
-
+*/
   // delete the heap memory
   delete [] array;
 }
